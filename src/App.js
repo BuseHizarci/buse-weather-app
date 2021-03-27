f890c5f2184eec46cfd7a8f61c5ef75510e5f1ab
Busehzrc <77810671+Busehzrc@users.noreply.github.com>
2021-03-27 22:52:40 +0000

    Linkedln and Github link added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Styles.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+         <Weather defaultCity="London" />
+         <footer>
+           This app Coded by {""}
+           <a href = "https://www.linkedin.com/in/buse-hizarci-258342111/" target="_blank">
+             Buse HIZARCI
+           </a>{" "}
+           and is {""}
+           <a href="https://github.com/BuseHizarci" target="_blank">
+             {""}
+           </a>
+         </footer>
+      </div>
     </div>
   );
 }
